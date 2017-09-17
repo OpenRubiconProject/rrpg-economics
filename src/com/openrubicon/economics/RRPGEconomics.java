39c5e3e1df49bf78d918d95798dfc2eb91646668
Shawn Clake <shawn.clake@gmail.com>
2017-09-17 15:09:36 -0600

    Registered as a module

--- src/com/openrubicon/economics/RRPGEconomics.java
+++ src/com/openrubicon/economics/RRPGEconomics.java
@@ -1,9 +1,16 @@
 package com.openrubicon.economics;
 
+import com.openrubicon.core.RRPGCore;
 import org.bukkit.plugin.java.JavaPlugin;
-import com.openrubicon.core.iModule;
+import com.openrubicon.core.interfaces.iModule;
 
 public class RRPGEconomics extends JavaPlugin implements iModule {
+
+    @Override
+    public void onLoad()
+    {
+        RRPGCore.modules.addModule(this);
+    }
 
     @Override
     public String getKey() {
